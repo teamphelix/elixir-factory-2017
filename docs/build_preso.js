4690509adc1ce5e5820cc634bbdbaec7d8f9fd44
Ari Lerner <arilerner@mac.com>
2017-03-21 01:49:42 -0700

    TODO: Remark

--- docs/build_preso.js
+++ docs/build_preso.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 
-const remark = require('remark').default
 const md2remark = require('md2remark');
 const fs = require('fs')
 
 const contents = fs.readFileSync(__dirname + "/elixabot.md");
 const markdown = contents.toString();
-
-const slideshow = remark.create();
 
 md2remark(markdown).then(function(slidesMarkdown) {
   console.log(slidesMarkdown);
